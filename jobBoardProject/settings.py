400be3775d34531fb80363f3705e9a2b6e607344
omraj0 <138387841+omraj0@users.noreply.github.com>
2025-11-04 01:52:54 +0530

    added staticfiles path in urls.py

--- jobBoardProject/settings.py
+++ jobBoardProject/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
